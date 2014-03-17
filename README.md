1fb875ea26dc918c528c9920c993c94d6103761e
Gregg Tavares <github@greggman.com>
2014-03-18 04:16:23 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,222 @@
+DeJson.NET
+==========
+
+A Deserializer from JSON to C# classes.
+
+Example:
+
+    using DeJson;
+
+    class Bar {
+        public int z;
+        public int a;
+    }
+    class Foo {
+        public int x;
+        public int y;
+        public Bar[] g;
+        public int[][] b;
+    }
+
+    void Test() {
+        // Make
+        Deserializer deserializer = new Deserializer();
+
+        string json = "{\"x\":123,\"y\":456,\"g\":[{\"z\":5,\"a\":12},{\"z\":4,\"a\":23},{\"z\":3,\"a\":34}],\"b\":[[1,2],[4,5,6],[7,6]]}";
+
+        Foo f = deserializer.Deserialize<Foo>(json);
+
+        Console.WriteLine("f.x          : " + f.x       );
+        Console.WriteLine("f.y          : " + f.y       );
+        Console.WriteLine("f.g[0].z     : " + f.g[0].z  );
+        Console.WriteLine("f.g[0].a     : " + f.g[0].a  );
+        Console.WriteLine("f.g[1].z     : " + f.g[1].z  );
+        Console.WriteLine("f.g[1].a     : " + f.g[1].a  );
+        Console.WriteLine("f.g[2].z     : " + f.g[2].z  );
+        Console.WriteLine("f.g[2].a     : " + f.g[2].a  );
+        Console.WriteLine("f.g.Length   : " + f.g.Length);
+        Console.WriteLine("f.b[0][0]    : " + f.b[0][0] );
+        Console.WriteLine("f.b[0][1]    : " + f.b[0][1] );
+        Console.WriteLine("f.b[1][0]    : " + f.b[1][0] );
+        Console.WriteLine("f.b[1][1]    : " + f.b[1][1] );
+        Console.WriteLine("f.b[1][2]    : " + f.b[1][2] );
+        Console.WriteLine("f.b[2][0]    : " + f.b[2][0] );
+        Console.WriteLine("f.b[2][1]    : " + f.b[2][1] );
+        Console.WriteLine("f.b.Length   : " + f.b.Length);
+        Console.WriteLine("f.b[0].Length: " + f.b[0].Length);
+        Console.WriteLine("f.b[1].Length: " + f.b[1].Length);
+        Console.WriteLine("f.b[2].Length: " + f.b[2].Length);
+    }
+
+Deserializing Derived Types
+---------------------------
+
+If you have some dervied type then you need to provide a CustomCreator to tell
+the Deserializer how to decide which derived type to create. Example
+
+    public class Fruit {
+      public int fruitType; // 0 = Apple, 1 = Raspberry
+    };
+
+    public class Apple : Fruit {
+        public float height;
+        public float radius;
+    };
+
+    public class Raspberry : Fruit {
+         public int numBulbs;
+    }
+
+    public class FruitCreator : Deserializer.CustomCreator {
+
+        // Called when the Deserializer realizes it needs a Fruit (abstract)
+        // so you get a chance to create a concrete instance of a Fruit (Apple, Raspberry)
+        public override object Create(Dictionary<string, object> src, Dictionary<string, object> parentSrc) {
+
+            // read the fruitType field
+            int fruitType = Convert.ToInt32(src["fruitType"]);
+            if (fruitType == 0) {
+                return new Apple();
+            } else if (fruitType == 1) {
+                return new Raspberry();
+            }
+            return null;
+        }
+
+        // Tell the Deserializer the base type we create.
+        public override System.Type TypeToCreate() {
+            return typeof(Fruit);
+        }
+    }
+
+...
+
+    void Test() {
+        Deserializer deserializer = new Deserializer();
+        deserializer.RegisterCreator(new FruitCreator());
+
+        string appleJson = "{\"fruitType\":0,\"height\":1.2,\"radius\":7}";
+        string raspberryJson = "{\"fruitType\":1,\"numBulbs\":27}";
+
+        Fruit apple = deserializer.Deserialize<Fruit>(appleJson);
+        Fruit raspberry = deserializer.Deserialize<Fruit>(raspberryJson);
+
+        Console.WriteLine("apple.fruitType:     " + apple.fruitType);
+        Console.WriteLine("apple.height:        " + ((Apple)apple).height);
+        Console.WriteLine("apple.radius:        " + ((Apple)apple).radius);
+        Console.WriteLine("raspberry.fruitType: " + raspberry.fruitType);
+        Console.WriteLine("raspberry.height:    " + ((Raspberry)raspberry).numBulbs);
+    }
+
+If the data to determine the type is in the containing class you can use `parentSrc` to
+inspect the parent's field in your creator. Example
+
+    public class Message {
+        public string msgType;
+        public MessageData data;
+    }
+
+    public class MessageData {
+    }
+
+    public class MessageDataMouseMove : MessageData {
+        public int x;
+        public int y;
+    }
+
+    public class MessageDataKeyDown : MessageData {
+        public int keyCode;
+    }
+
+    public class MessageDataCreator : Deserializer.CustomCreator {
+        // Called when the Deserializer realizes it needs a
+        // MessageData (abstract) so you get a chance to create
+        // a concrete instance of a MessageData (MessageDataMouseMove,
+        // MessageDataKeyDown)
+        public override object Create(Dictionary<string, object> src,
+                                      Dictionary<string, object> parentSrc) {
+
+            // read the msgType from Message
+            // parentSrc is the fields from Message
+            // src is the field from MessageData
+            string msgType = Convert.ToString(parentSrc["msgType"]);
+            if (msgType.Equals("mouseMove")) {
+                return new MessageDataMouseMove();
+            } else if (msgType.Equals("keyDown")) {
+                return new MessageDataKeyDown();
+            }
+            return null;
+        }
+
+        // Tell the Deserializer the base type we create.
+        public override System.Type TypeToCreate() {
+            return typeof(MessageData);
+        }
+    }
+
+...
+
+    void Test() {
+        Deserializer deserializer = new Deserializer();
+        deserializer.RegisterCreator(new MessageDataCreator());
+
+        string mouseMoveJson = "{\"msgType\":\"mouseMove\",\"data\":{\"x\":123,\"y\":456}}";
+        string keyDownJson = "{\"msgType\":\"keyDown\",\"data\":{\"keyCode\":789}}";
+
+        Message msg1 = deserializer.Deserialize<Message>(mouseMoveJson);
+        Message msg2 = deserializer.Deserialize<Message>(keyDownJson);
+
+        Console.WriteLine("msg1.msgType                            : " + msg1.msgType                           );
+        Console.WriteLine("((MessageDataMouseMove)msg1.data).x     : " + ((MessageDataMouseMove)msg1.data).x    );
+        Console.WriteLine("((MessageDataMouseMove)msg1.data).y     : " + ((MessageDataMouseMove)msg1.data).y    );
+        Console.WriteLine("msg2.msgType                            : " + msg2.msgType                           );
+        Console.WriteLine("((MessageDataKeyDown)msg2.data).keyCode : " + ((MessageDataKeyDown)msg2.data).keyCode);
+    }
+
+Notes:
+------
+
+I'm a C# noob but it seems to work for the cases I've thrown at it so far.
+
+This was written for consuming JSON provided by JavaScript in a browser
+
+Classes must have a no parameter constructor and all fields must be public.
+Consider these classes just for passing info through JSON.
+
+Why?
+----
+
+I tried to use JSON.NET but I needed it for Unity3D. Unity3D is apparently
+using .NET 2.0. JSON.NET supports .NET 2.0 but I couldn't get it to
+do. In particular I couldn't get it to handle the 2 advanced cases
+above. In JSON.NET you can apparently handle the first case where
+the field that determines the type is in the class being deserialized
+but unfortunately to do that it uses some advanced features of Linq
+that didn't seem to work in Unity3D.
+
+Of course maybe I just don't know what I'm doing and it would have worked
+and I could have saved a bunch of time but I couldn't get it to work
+and this didn't take too long to write.
+
+License
+-------
+
+It's the MIT license. See top of DeJson.cs
+
+
+To Do:
+------
+
+*   Consider updating to handle auto serializing type info.
+
+    If I modified MiniJSON to optionally output type info then I could also
+    modify the Deserializer to use that info. This would remove the need
+    for `CustomCreator`s for those cases when you're serializing from
+    C#
+
+
+
+
+
+
+
---
+++ src/DeJson.cs
@@ -0,0 +1,225 @@
+﻿/*
+ * Copyright 2014, Gregg Tavares.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Gregg Tavares. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
+
+namespace DeJson {
+
+public class Deserializer {
+
+    /// <summary>
+    /// A Class used to direct which class to make when it's not obvious, like if you have a class
+    /// with a member that's a base class but the actual class could be one of many derived classes.
+    /// </summary>
+    public abstract class CustomCreator {
+
+        /// <summary>
+        /// Creates an new derived class when a base is expected
+        /// </summary>
+        /// <param name="src">A dictionary of the json fields that belong to the object to be created.</param>
+        /// <param name="parentSrc">A dictionary of the json fields that belong to the object that is the parent of the object to be created.</param>
+        /// <example>
+        /// Example: Assume you have the following classes
+        /// <code>
+        ///     class Fruit { public int type; }
+        ///     class Apple : Fruit { public float height; public float radius; };
+        ///     class Raspberry : Fruit { public int numBulbs; }
+        /// </code>
+        /// You'd register a dervied CustomCreator for type `Fruit`. When the Deserialize needs to create
+        /// a `Fruit` it will call your Create function. Using `src` you could look at `type` and
+        /// decide whether to make an Apple or a Raspberry.
+        /// <code>
+        ///     int type = src["type"];
+        ///     if (type == 0) { return new Apple; }
+        ///     if (type == 1) { return new Raspberry; }
+        ///     ..
+        /// </code>
+        /// If the parent has info on the type you can do this
+        /// <code>
+        ///     class Fruit { }
+        ///     class Apple : Fruit { public float height; public float radius; };
+        ///     class Raspberry : Fruit { public int numBulbs; }
+        ///     class Basket { public int typeInBasket; Fruit fruit; }
+        /// </code>
+        /// In this case again, when trying to create a `Fruit` your CustomCreator.Create function
+        /// will be called. You can use `'parentSrc`' to look at the fields from 'Basket' as in
+        /// <code>
+        ///     int typeInBasket = parentSrc['typeInBasket'];
+        ///     if (type == 0) { return new Apple; }
+        ///     if (type == 1) { return new Raspberry; }
+        ///     ..
+        /// </code>
+        /// </example>
+        /// <returns>The created object</returns>
+        public abstract object Create(Dictionary<string, object> src, Dictionary<string, object> parentSrc);
+
+        /// <summary>
+        /// The base type this CustomCreator makes.
+        /// </summary>
+        /// <returns>The type this CustomCreator makes.</returns>
+        public abstract System.Type TypeToCreate();
+    }
+
+    /// <summary>
+    /// Deserializer for Json to your classes.
+    /// </summary>
+    public Deserializer() {
+        m_creators = new Dictionary<System.Type, CustomCreator>();
+    }
+
+    /// <summary>
+    /// Deserializes a json string into classes.
+    /// </summary>
+    /// <param name="json">String containing JSON</param>
+    /// <returns>An instance of class T.</returns>
+    /// <example>
+    /// <code>
+    ///     public class Foo {
+    ///         public int num;
+    ///         public string name;
+    ///         public float weight;
+    ///     };
+    ///
+    ///     public class Bar {
+    ///         public int hp;
+    ///         public Foo someFoo;
+    ///     };
+    /// ...
+    ///     Deserializer deserializer = new Deserializer();
+    ///
+    ///     string json = "{\"hp\":123,\"someFoo\":{\"num\":456,\"name\":\"gman\",\"weight\":156.4}}";
+    ///
+    ///     Bar bar = deserializer.Deserialize<Bar>(json);
+    ///
+    ///     print("bar.hp: " + bar.hp);
+    ///     print("bar.someFoo.num: " + bar.someFoo.num);
+    ///     print("bar.someFoo.name: " + bar.someFoo.name);
+    ///     print("bar.someFoo.weight: " + bar.someFoo.weight);
+    ///
+    /// </code>
+    /// </example>
+    public T Deserialize<T> (string json) where T : new() {
+        Dictionary<string, object> src = (Dictionary<string, object>)Json.Deserialize(json);
+        return DeserializeT<T>(src);
+    }
+
+    /// <summary>
+    /// Registers a CustomCreator.
+    /// </summary>
+    /// <param name="creator">The creator to register</param>
+    public void RegisterCreator(CustomCreator creator) {
+        System.Type t = creator.TypeToCreate();
+        m_creators[t] = creator;
+    }
+
+    private T DeserializeT<T>(Dictionary<string, object> src) where T : new() {
+        object o = DeserializeO(typeof(T), src, null);
+        return (T)o;
+    }
+
+    private object DeserializeO(Type destType, Dictionary<string, object> src, Dictionary<string, object> parentSrc) {
+        object dest = null;
+
+        CustomCreator creator;
+        if (m_creators.TryGetValue(destType, out creator)) {
+            dest = creator.Create(src, parentSrc);
+        }
+
+        if (dest == null) {
+            dest = Activator.CreateInstance(destType);
+        }
+
+        DeserializeIt(dest, src);
+        return dest;
+    }
+
+    private void DeserializeIt(object dest, Dictionary<string, object> src) {
+        System.Type type = dest.GetType();
+        System.Reflection.FieldInfo[] fields = type.GetFields();
+
+        DeserializeClassFields(dest, fields, src);
+    }
+
+    private void DeserializeClassFields(object dest, System.Reflection.FieldInfo[] fields, Dictionary<string, object> src) {
+        foreach (System.Reflection.FieldInfo info in fields) {
+
+            object value = src[info.Name];
+            DeserializeField(dest, info, value, src);
+
+        }
+    }
+
+    private void DeserializeField(object dest, System.Reflection.FieldInfo info, object value, Dictionary<string, object> src) {
+        Type fieldType = info.FieldType;
+        object o = ConvertToType(value, fieldType, src);
+        info.SetValue(dest, o);
+    }
+
+    private object ConvertToType(object value, System.Type type, Dictionary<string, object> src) {
+        if (type.IsArray) {
+            List<object> elements = (List<object>)value;
+            int numElements = elements.Count;
+            Type elementType = type.GetElementType();
+            Array array = Array.CreateInstance(elementType, numElements);
+            int index = 0;
+            foreach (object elementValue in elements) {
+                object o = ConvertToType(elementValue, elementType, src);
+                array.SetValue(o, index);
+                ++index;
+            }
+            return array;
+        } else if (type == typeof(string)) {
+            return Convert.ToString(value);
+        } else if (type == typeof(int)) {
+            return Convert.ToInt32(value);
+        } else if (type == typeof(float)) {
+            return Convert.ToSingle(value);
+        } else if (type == typeof(double)) {
+            return Convert.ToDouble(value);
+        } else if (type == typeof(bool)) {
+            return Convert.ToBoolean(value);
+        } else if (type.IsClass) {
+            return DeserializeO(type, (Dictionary<string, object>)value, src);
+        } else {
+            // Should we throw here?
+        }
+        return value;
+    }
+
+    private Dictionary<System.Type, CustomCreator> m_creators;
+};
+
+}  // namespace DeJson
+
+
